64655e9e30698be5d74ae639c4d3cc913a60f71a
楠z✰HisakoR <3389075400@qq.com>
2025-01-15 15:04:51 +0800

    初始化

--- dummyDoll/src/Main.java
+++ dummyDoll/src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("gui/main.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("dummyDoll");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.show();
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
